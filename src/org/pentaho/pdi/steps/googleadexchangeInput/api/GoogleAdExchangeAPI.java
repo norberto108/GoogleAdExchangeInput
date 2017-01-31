4c72b805e5da41e432fc95d8e0fd64c8ce0dc124
norberto108 <norberto108@gmail.com>
2017-01-31 15:38:23 -0200

    import declarations organized

--- src/org/pentaho/pdi/steps/googleadexchangeInput/api/GoogleAdExchangeAPI.java
+++ src/org/pentaho/pdi/steps/googleadexchangeInput/api/GoogleAdExchangeAPI.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 
 import org.pentaho.di.core.exception.KettleException;
